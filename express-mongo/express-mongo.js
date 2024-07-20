023885c646954520259e28c54e4d62ea315a2765
rahul <rahulmudgal53@gmail.com>
2024-07-20 21:13:17 +0530

    Initial commit

---
+++ express-mongo/express-mongo.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const bp = require('body-parser')
+const cors= require('cors')
+const mongoose = require('mongoose')
+const empc= require('./models/model')
+const URL = "mongodb+srv://admin:RAHUL12@cluster0.scl3pyh.mongodb.net/shopApp?retryWrites=true&w=majority&appName=Cluster0"
+
+var app = express()
+app.use(cors())
+app.use(bp.json())
+
+app.post('/addproduct', (req, res) => {
+    const user= new empc({...req.body})
+    user.save().then(()=> console.log('product added'))
+    res.send('product added')
+
+})
+app.get('/loaddata',async(req,res)=>{
+    const users= await empc.find();
+    res.send(users)
+})
+app.get('/products/:id',async(req,res)=>{
+    const uid=parseInt(req.params.id)
+    const users= await empc.findById(uid);
+    res.send(users)
+})
+const startServer = async () => {
+    await mongoose.connect(URL)
+    app.listen(4000, () => {
+        console.log('server is ready...!');
+    })
+}
+startServer()
